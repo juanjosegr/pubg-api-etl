331b5d1f22b57f3e29a15c4c3008fc98cf44f20d
juanjosegr <104174108+juanjosegr@users.noreply.github.com>
2025-05-07 12:52:14 +0200

    Add files via upload

---
+++ db_config.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+from urllib.parse import quote
+import os
+from dotenv import load_dotenv
+
+current_dir = os.path.dirname(__file__) 
+env_path = os.path.abspath(os.path.join(current_dir, "..", "config", "credenciales.env"))
+
+load_dotenv(env_path)
+
+
+def get_engine():
+    user = os.getenv("DB_USER")
+    raw_password = os.getenv("DB_PASS")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    dbname = os.getenv("DB_NAME")
+
+    password = quote(raw_password)
+    connection_string = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+
+    print("Conectando a la base de datos...")
+    try:
+        engine = create_engine(connection_string)
+        with engine.connect() as conn:
+            print("✅ Conexión a PostgreSQL exitosa.")
+        return engine
+    except OperationalError as e:
+        print("❌ Error al conectar a la base de datos:")
+        print(e)
+        return None
+
+engine_start = get_engine()
---
+++ pubg_api_config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+current_dir = os.path.dirname(__file__) 
+env_path = os.path.abspath(os.path.join(current_dir, "..", "config", "credenciales.env"))
+
+load_dotenv(env_path)
+
+api_key = os.getenv("PUBG_API_KEY")
+
+HEADERS = {
+    "Authorization": f"Bearer {api_key}",
+    "Accept": "application/vnd.api+json"
+}
+
+BASE_URL = "https://api.pubg.com/shards/steam"
+
