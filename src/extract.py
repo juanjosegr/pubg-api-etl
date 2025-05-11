f29a6cd8362557c26e04f3066e86f3deac141de6
juanjosegr <juanjosegr9@gmail.com>
2025-05-11 13:18:21 +0200

    Todos lo endpoints creado y refactorización de código.

--- src/extract.py
+++ src/extract.py
@@ -8,96 +8,40 @@ sys.path.append(config_path)
 
 from pubg_api_config import BASE_URL, HEADERS
 
-
-
 def searchPlayer (player_name):
     # Endpoint to find player for name
     url = f"{BASE_URL}/players?filter[playerNames]={player_name}"
-    
     # Send the GET request to the API with authorization headers
     response = requests.get(url, headers=HEADERS)
-
     # Raise an exception if the request failed (e.g., 404 or 500)
     response.raise_for_status()    
-
     #Save the information and transform with Json library and return the player id
     data = response.json()
     return data["data"][0]["id"]
 
 def find_match_players(player_id):
-    # Endpoint to obtain data of the player from id
     url = f"{BASE_URL}/players/{player_id}"
-
-    # Send the GET request to the API with authorization headers
     response = requests.get(url, headers=HEADERS)
-    
-    # Raise an exception if the request failed (e.g., 404 or 500)
     response.raise_for_status()  
-
-    # Returns all of matches from the player
     return response.json()["data"]["relationships"]["matches"]["data"]
 
-
 def extract_match_details(match_id):
-    # Construct the API endpoint URL using the match ID
     url = f"{BASE_URL}/matches/{match_id}"
-    
-    # Send the GET request to the API with authorization headers
     response = requests.get(url, headers=HEADERS)
-    
-    # Raise an exception if the request failed (e.g., 404 or 500)
     response.raise_for_status()
-    
-    # Return the JSON response containing match details
     return response.json()
 
-def download_telemetry(match_id):
+def find_telemetry(match_id):
     url = f"{BASE_URL}/matches/{match_id}"
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
-    match_data = response.json()
-
-    # Obtener asset ID de telemetría
-    asset_id = match_data['data']['relationships']['assets']['data'][0]['id']
-
-    # Buscar el asset en el array 'included'
-    asset = next((item for item in match_data['included'] if item['type'] == 'asset' and item['id'] == asset_id), None)
-    if asset is None:
-        print("❌ Telemetry asset not found.")
-        return match_data
-
-    telemetry_url = asset['attributes']['URL']
-    
-    tele_response = requests.get(telemetry_url, headers={"Accept-Encoding": "gzip"})
-    tele_response.raise_for_status()
-
-    os.makedirs("telemetry", exist_ok=True)
-    filepath = f"telemetry/{match_id}_telemetry.json"
-    with open(filepath, "wb") as f:
-        f.write(tele_response.content)
-
-    print(f"✅ Telemetría guardada en {filepath}")
-
-
-import pandas as pd
-import time
+    return response.json()
 
 def find_seasons():
     url = f"{BASE_URL}/seasons"
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
     return response.json()
-
-def listar_temporadas():
-    data = find_seasons()
-    seasons = []
-    for season in data['data']:
-        seasons.append({
-            "ID:" : season['id'],
-            "is_current" : season['attributes']['isCurrentSeason'],
-            "is_offseason" : season['attributes']['isOffseason']
-        })
-    return pd.DataFrame(seasons)
 
 def ranked_stats(player_id,seasonID):
     url = f"{BASE_URL}/players/{player_id}/seasons/{seasonID}/ranked"
@@ -111,48 +55,50 @@ def normal_stats(player_id,seasonID):
     response.raise_for_status()
     return response.json()
 
-import time
+def stats_lifetime(player_id):
+    url = f"{BASE_URL}/players/{player_id}/seasons/lifetime"
+    response = requests.get(url, headers = HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-def comprobar_stats_en_todas_las_seasons(player_id, delay=6.2):
-    df_seasons = listar_temporadas()
+def weapon_mastery(player_id):
+    url = f"{BASE_URL}/players/{player_id}/weapon_mastery"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-    for index, row in df_seasons.iterrows():
-        season_id = row["ID:"]
-        
-        # Filtrar temporadas de consola
-        if any(x in season_id.lower() for x in ["console", "psn", "xbox"]):
-            print(f"⏩ Saltando temporada de consola: {season_id}")
-            continue
+def survival_mastery(player_id):
+    url = f"{BASE_URL}/players/{player_id}/survival_mastery"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-        print(f"\n🔎 Comprobando season: {season_id}")
+def find_clan(clan_id):
+    url = f"{BASE_URL}/clans/{clan_id}"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-        # Comprobar stats normales
-        try:
-            normal_data = normal_stats(player_id, season_id)
-            normal_stats_data = normal_data.get("data", {}).get("attributes", {}).get("gameModeStats", {})
+def obtain_leaderboards(platform_region, seasonId, gameMode):
+    url = f"https://api.pubg.com/shards/{platform_region}/leaderboards/{seasonId}/{gameMode}"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-            if normal_stats_data:
-                print("✅ Normales: Stats encontradas.")
-            else:
-                print("⚠️ Normales: Sin stats.")
-        except requests.exceptions.HTTPError as e:
-            print(f"❌ Normales: Error HTTP - {e}")
-        
-        time.sleep(delay)  # Espera entre peticiones
+def obtain_samples():
+    url = f"{BASE_URL}/samples"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-        # Comprobar stats ranked
-        try:
-            ranked_data = ranked_stats(player_id, season_id)
-            ranked_stats_data = ranked_data.get("data", {}).get("attributes", {}).get("rankedGameModeStats", {})
+def obtain_status():
+    url = "https://api.pubg.com/status"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
 
-            if ranked_stats_data:
-                print("✅ Ranked: Stats encontradas.")
-            else:
-                print("⚠️ Ranked: Sin stats.")
-        except requests.exceptions.HTTPError as e:
-            print(f"❌ Ranked: Error HTTP - {e}")
-        
-        time.sleep(delay)  # Espera entre peticiones
-
-player_id = "account.4f2c09f2b4d447379dbace96bc582a96"
-comprobar_stats_en_todas_las_seasons(player_id)
+def obtain_tournament_stats():
+    url = "https://api.pubg.com/tournaments"
+    response = requests.get(url,headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
--- src/transform.py
+++ src/transform.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import os
+import requests
+import time
+from extract import find_telemetry, find_seasons, normal_stats, ranked_stats
 
 def match_info(match_data):
     data = match_data['data']['attributes']
@@ -52,3 +56,91 @@ def player_match_info(match_data):
         })
     pd.set_option('display.max_rows', None)
     return pd.DataFrame(rows).sort_values(by=['winPlace', 'kills'], ascending=[True, False]).reset_index(drop=True)
+
+def transform_telemetry(match_id):
+    match_data  = find_telemetry(match_id)
+
+    # Obtener asset ID de telemetría
+    try:
+        asset_id = match_data['data']['relationships']['assets']['data'][0]['id']
+    except (KeyError, IndexError):
+        print("❌ No se encontró asset_id en los datos.")
+        return match_data
+    
+    # Buscar el asset en el array 'included'
+    asset = next(
+        (item for item in match_data.get('included',[])
+         if item['type'] == 'asset' and item['id'] == asset_id), 
+        None)
+
+    if asset is None:
+        print("❌ Telemetry asset not found.")
+        return match_data
+
+    telemetry_url = asset['attributes'].get('URL')
+    if not telemetry_url:
+        print("❌ URL Telemetry not found.")
+        return match_data
+    
+    tele_response = requests.get(telemetry_url, headers={"Accept-Encoding": "gzip"})
+    tele_response.raise_for_status()
+
+    os.makedirs("data/telemetry", exist_ok=True)
+    filepath = f"data/telemetry/{match_id}_telemetry.json"
+    with open(filepath, "wb") as f:
+        f.write(tele_response.content)
+
+    print(f"✅ Telemetría guardada en {filepath}")
+    return filepath
+
+def list_seasons():
+    data = find_seasons()
+    seasons = []
+    for season in data['data']:
+        seasons.append({
+            "ID:" : season['id'],
+            "is_current" : season['attributes']['isCurrentSeason'],
+            "is_offseason" : season['attributes']['isOffseason']
+        })
+    return pd.DataFrame(seasons)
+
+def found_all_season_stats(player_id, delay=6.2):
+    df_seasons = list_seasons()
+
+    for index, row in df_seasons.iterrows():
+        season_id = row["ID:"]
+        
+        # Filtrar temporadas de consola
+        if any(x in season_id.lower() for x in ["console", "psn", "xbox"]):
+            print(f"⏩ Saltando temporada de consola: {season_id}")
+            continue
+
+        print(f"\n🔎 Comprobando season: {season_id}")
+
+        # Comprobar stats normales
+        try:
+            normal_data = normal_stats(player_id, season_id)
+            normal_stats_data = normal_data.get("data", {}).get("attributes", {}).get("gameModeStats", {})
+
+            if normal_stats_data:
+                print("✅ Normales: Stats encontradas.")
+            else:
+                print("⚠️ Normales: Sin stats.")
+        except requests.exceptions.HTTPError as e:
+            print(f"❌ Normales: Error HTTP - {e}")
+        
+        time.sleep(delay)  # Espera entre peticiones
+
+        # Comprobar stats ranked
+        try:
+            ranked_data = ranked_stats(player_id, season_id)
+            ranked_stats_data = ranked_data.get("data", {}).get("attributes", {}).get("rankedGameModeStats", {})
+
+            if ranked_stats_data:
+                print("✅ Ranked: Stats encontradas.")
+            else:
+                print("⚠️ Ranked: Sin stats.")
+        except requests.exceptions.HTTPError as e:
+            print(f"❌ Ranked: Error HTTP - {e}")
+        
+        time.sleep(delay)  # Espera entre peticiones
