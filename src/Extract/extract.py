7bc9fa4634d339931039a9355e3915c33d43a280
juanjosegr <juanjosegr9@gmail.com>
2025-05-12 16:03:57 +0200

    Refactorización de código

--- src/Extract/extract.py
+++ src/Extract/extract.py
@@ -2,87 +2,46 @@ import sys
 import os
 import requests
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from config.pubg_api_config import BASE_URL, HEADERS
+from config.pubg_api_config import BASE_URL, HEADERS, get_api_response
 
 def searchPlayer (player_name):
-    # Endpoint to find player for name
-    url = f"{BASE_URL}/players?filter[playerNames]={player_name}"
-    # Send the GET request to the API with authorization headers
-    response = requests.get(url, headers=HEADERS)
-    # Raise an exception if the request failed (e.g., 404 or 500)
-    response.raise_for_status()    
-    #Save the information and transform with Json library and return the player id
-    data = response.json()
-    return data["data"][0]["id"]
+    return get_api_response(f"players?filter[playerNames]={player_name}")["data"][0]["id"]
 
 def find_match_players(player_id):
-    url = f"{BASE_URL}/players/{player_id}"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()  
-    return response.json()["data"]["relationships"]["matches"]["data"]
+    return get_api_response(f"players/{player_id}")["data"]["relationships"]["matches"]["data"]
 
 def extract_match_details(match_id):
-    url = f"{BASE_URL}/matches/{match_id}"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"matches/{match_id}")
 
 def find_telemetry(match_id):
-    url = f"{BASE_URL}/matches/{match_id}"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"matches/{match_id}")
 
 def find_seasons():
-    url = f"{BASE_URL}/seasons"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response("seasons")
 
 def ranked_stats(player_id,seasonID):
-    url = f"{BASE_URL}/players/{player_id}/seasons/{seasonID}/ranked"
-    response = requests.get(url, headers = HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"players/{player_id}/seasons/{seasonID}/ranked")
 
 def normal_stats(player_id,seasonID):
-    url = f"{BASE_URL}/players/{player_id}/seasons/{seasonID}"
-    response = requests.get(url, headers = HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"players/{player_id}/seasons/{seasonID}")
 
 def stats_lifetime(player_id):
-    url = f"{BASE_URL}/players/{player_id}/seasons/lifetime"
-    response = requests.get(url, headers = HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"players/{player_id}/seasons/lifetime")
 
 def weapon_mastery(player_id):
-    url = f"{BASE_URL}/players/{player_id}/weapon_mastery"
-    response = requests.get(url,headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"players/{player_id}/weapon_mastery")
 
 def survival_mastery(player_id):
-    url = f"{BASE_URL}/players/{player_id}/survival_mastery"
-    response = requests.get(url,headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return get_api_response(f"players/{player_id}/survival_mastery")
 
 def find_clan(clan_id):
-    url = f"{BASE_URL}/clans/{clan_id}"
-    response = requests.get(url,headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
+   return get_api_response(f"clans/{clan_id}")
+
+def obtain_samples():
+    return get_api_response("samples")
 
 def obtain_leaderboards(platform_region, seasonId, gameMode):
     url = f"https://api.pubg.com/shards/{platform_region}/leaderboards/{seasonId}/{gameMode}"
-    response = requests.get(url,headers=HEADERS)
-    response.raise_for_status()
-    return response.json()
-
-def obtain_samples():
-    url = f"{BASE_URL}/samples"
     response = requests.get(url,headers=HEADERS)
     response.raise_for_status()
     return response.json()
--- src/config/pubg_api_config.py
+++ src/config/pubg_api_config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-
+import requests
 current_dir = os.path.dirname(__file__) 
 env_path = os.path.abspath(os.path.join(current_dir, "..", "config", "credenciales.env"))
 
@@ -14,3 +14,26 @@ HEADERS = {
 }
 
 BASE_URL = "https://api.pubg.com/shards/steam"
+
+def get_api_response(endPoint: str) -> dict:
+    """
+    Sends a GET request to the PUBG API and returns the JSON response.
+
+    Args:
+        endPoint (str): The API endpoint to query (appended to BASE_URL).
+
+    Returns:
+        dict: The parsed JSON response from the API.
+
+    Raises:
+        HTTPError: If the API response contains an HTTP error status.
+    """
+
+    # Build the full URL with the given endpoint
+    url = f"{BASE_URL}/{endPoint}"
+    # Send a GET request with authorization headers
+    response = requests.get(url, headers=HEADERS)
+    # Raise an exception if the request fails (e.g., 404 or 500)
+    response.raise_for_status()
+    # Return the response parsed as JSON
+    return response.json()
